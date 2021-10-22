9fd0e5a532ad00d08daf8f122352631260c94d90
Rafael Lima <Rafael.LA.Lima@ctw.bmwgroup.com>
2021-10-22 15:05:15 +0100

    Use Shot

--- app/src/androidTest/java/com/soutoss/facebooksnapshotsample/MainActivityTest.kt
+++ app/src/androidTest/java/com/soutoss/facebooksnapshotsample/MainActivityTest.kt
@@ -1,19 +1,15 @@
 package com.soutoss.facebooksnapshotsample
 
-import androidx.test.rule.ActivityTestRule
-import com.facebook.testing.screenshot.Screenshot
-import org.junit.Rule
+import androidx.test.core.app.ActivityScenario
+import com.karumi.shot.ScreenshotTest
 import org.junit.Test
 
-class MainActivityTest {
-
-    @get:Rule
-    var activityTestRule = ActivityTestRule(MainActivity::class.java, false, false)
-
+class MainActivityTest : ScreenshotTest {
     @Test
     fun shouldLoadListWithExpectedContent() {
-        val activity = activityTestRule.launchActivity(null)
-        Screenshot.snapActivity(activity).record()
+        ActivityScenario.launch(MainActivity::class.java).onActivity {
+            compareScreenshot(it)
+        }
     }
 
 }
